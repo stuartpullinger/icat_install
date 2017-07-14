7b0355fc16b95542288f72f03d38c314bb3422cc
Pullinger <stuart.pullinger@stfc.ac.uk>
2017-07-14 13:38:08 +0100

    Added init configuration to provisioning so mysqld and glassfish run on start up

--- bootstrap.sh
+++ bootstrap.sh
@@ -55,8 +55,45 @@ mysqladmin --user=root --password=pw create icat
 mysql --user=root --password=pw --execute="CREATE USER 'icat'@'localhost' IDENTIFIED BY 'icat';"
 mysql --user=root --password=pw --execute="GRANT ALL PRIVILEGES ON *.* TO 'icat'@'localhost';"
 
+# Configure the database to run on system start up
+chkconfig mysqld on
+
 # Create system account for glassfish
 useradd $GLASSFISH_USER
+
+# Configure Glassfish to run on system start up
+echo '#!/bin/sh
+
+# chkconfig: - 99 01
+
+### BEGIN INIT INFO
+# Provides:          glassfish
+# Required-Start:    $remote_fs $syslog $local_fs $network
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Starts a glassfish server
+### END INIT INFO
+
+GLASSFISH_HOME=/home/glassfish/glassfish4
+DOMAIN=localhost
+
+case "$1" in
+start)
+    sudo -u glassfish $GLASSFISH_HOME/bin/asadmin start-domain $DOMAIN >/dev/null
+    ;;
+stop)
+    sudo -u glassfish $GLASSFISH_HOME/bin/asadmin stop-domain $DOMAIN >/dev/null
+    ;;
+restart)
+    stop
+    start
+    ;;
+\*)
+    echo "usage: $0 (start|stop|restart|help)"
+esac' > /etc/init.d/glassfish
+chmod 0755 /etc/init.d/glassfish
+chkconfig --add glassfish
 
 # Continue setup and install as glassfish user
 su --command='cd $HOME; /vagrant/icat_install.sh' $GLASSFISH_USER
